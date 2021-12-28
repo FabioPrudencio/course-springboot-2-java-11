97f6a6b9183d730628632def64104379787c81ec
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-12-28 17:07:55 -0300

    Category

---
+++ src/main/java/com/ornitorrincosystems/coursespring/services/CategoryService.java
@@ -0,0 +1,31 @@
+package com.ornitorrincosystems.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ornitorrincosystems.coursespring.entities.Category;
+import com.ornitorrincosystems.coursespring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public void saveAll(List<Category> list) {
+		repository.saveAll(list);
+	}
+
+}
