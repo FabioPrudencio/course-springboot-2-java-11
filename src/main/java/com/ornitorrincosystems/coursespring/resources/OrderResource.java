c8bf191c486d5bd520ff0dd8df2fe8c66d2f7f40
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-12-29 18:46:34 -0300

    Subtotal & Total methods

--- src/main/java/com/ornitorrincosystems/coursespring/resources/OrderResource.java
+++ src/main/java/com/ornitorrincosystems/coursespring/resources/OrderResource.java
@@ -15,20 +15,20 @@ import com.ornitorrincosystems.coursespring.services.OrderService;
 @RestController
 @RequestMapping(value = "/orders")
 public class OrderResource {
-	
+
 	@Autowired
 	private OrderService service;
 
 	@GetMapping
 	public ResponseEntity<List<Order>> findAll() {
-		List<Order> list = service.findAll();		
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = service.findById(id);		
+		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}	
+	}
 
 }
